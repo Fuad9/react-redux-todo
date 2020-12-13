9ded168069f73c37362bcece917ddcd06722c9f4
Fuad9 <alfuad8548@gmail.com>
2020-12-13 08:34:42 +0600

    redux integrated

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deleteTodo, updateTodo } from "./redux/actions";
 
-const TodoItem = () => {
+const TodoItem = ({ todo }) => {
+   const dispatch = useDispatch();
+   const [editTodo, setEditTodo] = useState(false);
+   const [todoName, setTodoName] = useState(todo.name);
+
    return (
       <div>
          <div className="row mx-2 align-items-center">
-            <div>#1</div>
+            <div>#{todo.id.length > 1 ? todo.id[0] : todo.id}</div>
+
             <div className="col">
-               <h4>Todo Title</h4>
+               {editTodo ? (
+                  <input
+                     onChange={(e) => setTodoName(e.target.value)}
+                     type="text"
+                     className="form-control"
+                     value={todoName}
+                  />
+               ) : (
+                  <h4>{todo.name}</h4>
+               )}
             </div>
-            <div className="btn btn-warning m-2">Edit</div>
-            <div className="btn btn-danger m-2">Delete</div>
+
+            <button
+               onClick={() => {
+                  dispatch(
+                     updateTodo({
+                        ...todo,
+                        name: todoName,
+                     })
+                  );
+                  editTodo && setTodoName(todo.name);
+                  setEditTodo(!editTodo);
+               }}
+               className="btn btn-danger m-2"
+            >
+               {editTodo ? "Update" : "Edit"}
+            </button>
+
+            <button onClick={() => dispatch(deleteTodo(todo.id))} className="btn btn-warning m-2">
+               Delete
+            </button>
          </div>
       </div>
    );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
+   const todos = useSelector((state) => state);
+
    return (
       <div className="my-4">
-         <TodoItem />
-         <TodoItem />
-         <TodoItem />
-         <TodoItem />
+         {todos.map((todo) => {
+            return <TodoItem key={todo.id} todo={todo} />;
+         })}
       </div>
    );
 };
